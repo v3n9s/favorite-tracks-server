8c61249a6927d235ab836d5ba8e4423449f307c0
Venyamin Shvets <venyashvets@gmail.com>
2024-10-12 18:10:31 +0300

    init

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+// @ts-check
+import path from "node:path";
+import url from "node:url";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  files: ["src/**/*.ts", "eslint.config.js"],
+  extends: [
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylistic,
+  ],
+  languageOptions: {
+    parserOptions: {
+      projectService: {
+        allowDefaultProject: ["eslint.config.js"],
+      },
+      tsconfigRootDir: path.dirname(url.fileURLToPath(import.meta.url)),
+    },
+  },
+  rules: {
+    // stylistic
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+
+    // additional
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        allowDirectConstAssertionInArrowFunctions: false,
+        allowExpressions: true,
+        allowHigherOrderFunctions: false,
+      },
+    ],
+    "@typescript-eslint/strict-boolean-expressions": ["error"],
+  },
+});
