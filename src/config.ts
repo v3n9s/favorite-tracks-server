a9a73ea5e717d11984b33d63553c4c4fad3c7157
Venyamin Shvets <venyashvets@gmail.com>
2024-10-25 16:00:03 +0300

    implement config

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+const safeUint = z
+  .number({ coerce: true })
+  .int()
+  .min(0)
+  .max(Number.MAX_SAFE_INTEGER);
+
+const configSchema = z.object({
+  accessTokenSecret: z.string(),
+  accessTokenExpirationTime: safeUint,
+  refreshTokenSecret: z.string(),
+  refreshTokenExpirationTime: safeUint,
+  databaseFilePath: z.string(),
+  port: safeUint,
+});
+
+export type Config = z.output<typeof configSchema>;
+
+const configParseResult = configSchema.safeParse(process.env);
+
+if (configParseResult.error) {
+  console.error("error occured during config validation");
+  console.log(configParseResult.error.issues);
+  process.exit(1);
+}
+
+export const config: Config = configParseResult.data;
