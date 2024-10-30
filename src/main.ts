0eb6ff4dacee039e060f93e5a7f41cdbcdbdf25a
Venyamin Shvets <venyashvets@gmail.com>
2024-10-30 20:37:29 +0300

    require content-type header

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,19 @@ import { usersRouter } from "./users.controller.js";
 
 const app = express();
 
-app.use(json());
-
 app.use((req, res, next) => {
   res.removeHeader("x-powered-by");
   res.setHeader("content-type", "application/json");
+  if (req.headers["content-type"] !== "application/json") {
+    res.status(400).json({
+      error: 'content-type header should be set to "application/json"',
+    });
+    return;
+  }
   next();
 });
+
+app.use(json());
 
 app.use("/users", usersRouter);
 
